7c28c4a096fd19cffd1eb494de5ebdcd50ebec83
exuteboll <exuteboll4@gmail.com>
2025-02-02 19:37:24 +0200

    Add files via upload

---
+++ ConsoleApp11/Calculate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Calculate
+    {
+
+        public double Number1;
+        public double Number2;
+        public double rez;
+        public Calculate(){}
+        public Calculate(double Number1)
+        {
+            this.Number1 = Number1;
+        }
+        public Calculate(double Number1,double Number2) :this(Number1)
+        {
+            this.Number2 = Number2;
+        }
+        
+        public double Calculatekoren()
+        {
+            return Math.Sqrt(Number1 * Number2);
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Первое число: {Number1}\t второе число: {Number2}\t Результат{Calculatekoren():f2}");
+        }
+        public string Tostring()
+        {
+            return $"Первое число: {Number1}\t второе число: {Number2}\t Результат{Calculatekoren():f2}";
+        }
+    }
+}
